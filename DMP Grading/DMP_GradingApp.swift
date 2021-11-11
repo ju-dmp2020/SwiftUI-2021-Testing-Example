cd9ebb09e2b3f8cc53f9cc60a3499601567a8704
Garrit <garrit.schaap@ju.se>
2021-11-11 09:13:17 +0100

    Initial Commit

---
+++ DMP Grading/DMP_GradingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DMP_GradingApp.swift
+//  DMP Grading
+//
+//  Created by Garrit Schaap on 2021-11-11.
+//
+
+import SwiftUI
+
+@main
+struct DMP_GradingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DMP GradingTests/DMP_GradingTests.swift
@@ -0,0 +1,33 @@
+//
+//  DMP_GradingTests.swift
+//  DMP GradingTests
+//
+//  Created by Garrit Schaap on 2021-11-11.
+//
+
+import XCTest
+@testable import DMP_Grading
+
+class DMP_GradingTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
